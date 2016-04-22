6a2dae9f2911066252b2fe722519037bca0e5253
David Crespo <davidcrespoarroyo@gmail.com>
2016-04-22 15:38:42 +0100

    Added implementation for the two functions. Corrected tests.

--- functionalScalaCourse1/example/src/test/scala/example/ListsSuite.scala
+++ functionalScalaCourse1/example/src/test/scala/example/ListsSuite.scala
@@ -19,7 +19,7 @@ import org.scalatest.junit.JUnitRunner
  * inside eclipse using the built-in JUnit test runner.
  *
  * You have two options for running this test suite:
- * 
+ *
  * - Start the sbt console and run the "test" command
  * - Right-click this file in eclipse and chose "Run As" - "JUnit Test"
  */
@@ -47,7 +47,7 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 == 2) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -72,7 +72,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 === 2) // Fix me, please!
   }
 
 
@@ -99,9 +99,9 @@ class ListsSuite extends FunSuite {
    * Now we finally write some tests for the list functions that have to be
    * implemented for this assignment. We fist import all members of the
    * `List` object.
-   */ 
+   */
   import Lists._
-  
+
 
   /**
    * We only provide two very basic tests for you. Write more tests to make
@@ -117,7 +117,7 @@ class ListsSuite extends FunSuite {
   test("sum of a few numbers") {
     assert(sum(List(1,2,0)) === 3)
   }
-  
+
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
   }
