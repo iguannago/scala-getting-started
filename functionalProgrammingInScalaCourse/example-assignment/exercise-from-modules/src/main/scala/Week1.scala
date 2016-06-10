2bbc235807cac80113ad574d5ffe7c070cb221de
David Crespo <davidcrespoarroyo@gmail.com>
2016-06-10 18:37:43 +0100

    Added scala project for exercises from the lectures

---
+++ functionalProgrammingInScalaCourse/example-assignment/exercise-from-modules/src/main/scala/Week1.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by davicres on 10/06/2016.
+  */
+object Week1 {
+
+}
---
+++ functionalProgrammingInScalaCourse/example-assignment/exercise-from-modules/src/test/scala/Week1Test.scala
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith
+
+/**
+  * Created by davicres on 10/06/2016.
+  */
+@RunWith(classOf[JUnitRunner])
+class Week1Test extends FunSuite {
+
+}
