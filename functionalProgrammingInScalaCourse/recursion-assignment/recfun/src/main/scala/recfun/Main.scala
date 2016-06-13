b4ac34d0f1475070d459ed9bc82dcffb77976d50
David Crespo <davidcrespoarroyo@gmail.com>
2016-06-13 18:33:58 +0100

    Added CountChangeSuite function

--- functionalProgrammingInScalaCourse/recursion-assignment/recfun/src/main/scala/recfun/Main.scala
+++ functionalProgrammingInScalaCourse/recursion-assignment/recfun/src/main/scala/recfun/Main.scala
@@ -46,5 +46,15 @@ object Main {
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money > 0) {
+      if (coins.nonEmpty) {
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      }
+      else 0
+    }
+    else if (money == 0) 1
+    else 0
+  }
+
 }
