562e0dded4525e95244ae80a6a07c59a2d8be5a8
david.crespo <david.crespo@hmcts.net>
2022-09-21 12:23:03 +0100

    symbol method

--- scala-at-light-speed/src/main/scala/com/rockthejvm/ObjectOrientation.scala
+++ scala-at-light-speed/src/main/scala/com/rockthejvm/ObjectOrientation.scala
@@ -3,14 +3,18 @@ package com.rockthejvm
 object ObjectOrientation extends App {
 
   // class and instance
-  class Animal {
+  abstract class Animal {
+    def ?(str: String): Boolean
+
     //define fields and methods
     val age: Int = 0
 
     def eat(): Unit = println(s"Animal is $age years old")
   }
 
-  val anAnimal = new Animal
+  val anAnimal = new Animal {
+    override def ?(str: String): Boolean = ???
+  }
   anAnimal.eat()
 
   //inheritance
@@ -18,6 +22,8 @@ object ObjectOrientation extends App {
     override val age: Int = 10
 
     override def eat(): Unit = println(s"Dog with name $name is $age years old")
+
+    override def ?(str: String): Boolean = ???
   }
 
   // constructor arguments are NOT fields. We need to put val before the constructor name
@@ -30,6 +36,8 @@ object ObjectOrientation extends App {
     override val age: Int = 15
 
     override def eat(): Unit = println(s"Cat with name $name is $age years old")
+
+    override def ?(str: String): Boolean = ???
   }
 
   val aCat: Animal = new Cat("Tom")
@@ -57,17 +65,21 @@ object ObjectOrientation extends App {
   //interface
   trait Carnivore {
     def eat(): Unit
-
   }
 
   // single-class inheritance, multi-trait mixing
   class Leon extends Animal with Carnivore {
     override def eat(): Unit = println("I am a Leon a Carnivore animal")
 
+    override def ?(str: String): Boolean = {
+      if (str.equals("David")) true
+      else false
+    }
   }
 
   val aLeon: Animal = new Leon
   println(aLeon.eat())
+  println(aLeon ? "David")
 
   //anonymous classes
 
